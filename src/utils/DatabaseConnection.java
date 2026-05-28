c56807c1140b305bf6cd82d6c1cde22262145252
loemheng840 <limheng7676@gmail.com>
2026-05-28 12:43:44 +0700

    update main view

--- src/utils/DatabaseConnection.java
+++ src/utils/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/Enrollment";
-    private static final String USER = "postgres";
-    private static final String PASS = "8990";
+    private static final String URL = "jdbc:postgresql://localhost:5432/database";
+    private static final String USER = "username";
+    private static final String PASS = "password";
     private static Connection connection;
 
     public static Connection getConnection() {
